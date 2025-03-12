3c005d0d032b8c35a557a6b6a5b441f83074633b
Zahra Farajihormozi <79403254+zahrafarajih@users.noreply.github.com>
2025-03-12 11:43:12 +0100

    Updating CV

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing", {
-    strings: ["UI Designer", "UX Designer", "UX Researcher"],
-    typeSpeed: 100,
-    BackSpeed: 60,
+    strings: ["Web Designer","UI/UX Designer", "UI Developer", "UX Researcher"],
+    typeSpeed: 60,
+    BackSpeed: 40,
     loop: true
 })
 
 document.getElementById('downloadButton').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'Zahra-CV.pdf'; // Path to your PDF file
-    link.download = 'Zahra-CV.pdf'; // The name to save the file as
+    link.href = 'Zahra-Faraji-CV.pdf'; // Path to your PDF file
+    link.download = 'Zahra-Faraji-CV.pdf'; // The name to save the file as
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 });
 document.getElementById('downloadButtonIt').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'Zahra-Italian-CV.pdf';
-    link.download = 'Zahra-Italian-CV.pdf';
+    link.href = 'ZahraFaraji-CV(IT).pdf';
+    link.download = 'ZahraFaraji-CV(IT).pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
